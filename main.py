662079b8143c2cfd50b7338058293fd71131ab79
Linzjian666 <linzhenjian634@outlook.com>
2024-06-05 17:32:20 +0800

    update

---
+++ main.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+'''
+Author: Linzjian666
+Date: 2024-06-05 16:37:09
+LastEditors: Linzjian666
+LastEditTime: 2024-06-05 17:32:13
+'''
+import yaml
+
+def read_profile(input_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        profile = yaml.safe_load(f)
+    return profile
+
+def write_profile(output_file, profile):
+    with open(output_file, 'w', encoding='utf-8') as f:
+        yaml.dump(profile, f, sort_keys=False, allow_unicode=True)
+
+def dedup_rules(rules):
+    unique_rules = []
+    deduped_rules = []
+    for rule in rules:
+        rule_key = ','.join(rule.split(',')[:2])
+        if rule_key not in unique_rules:
+            deduped_rules.append(rule)
+            unique_rules.append(rule_key)
+    return deduped_rules
+
+if __name__ == "__main__":
+    input_file = input('请输入配置文件路径: ')
+    output_file = input('请输入输出文件路径: ')
+    profile = read_profile(input_file)
+    rules = profile.get('rules', [])
+    deduped_rules = dedup_rules(rules)
+    profile['rules'] = deduped_rules
+    write_profile(output_file, profile)
